a2b727c26da19536845c25e15cb3620f89e276ff
songsoo <ssh102525@naver.com>
2021-11-12 12:46:20 +0900

    New Version!

--- scale_encode.py
+++ scale_encode.py
@@ -1,63 +1,166 @@
+import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 
-#onehot encoding
-def onehotEncode(df, name):
-   le = preprocessing.OneHotEncoder(handle_unknown='ignore')
-   enc = df[[name]]
-   enc = le.fit_transform(enc).toarray()
-   enc_df = pd.DataFrame(enc, columns=le.categories_[0])
-   df.loc[:, le.categories_[0]] = enc_df
-   df.drop(columns=[name], inplace=True)
 
-#label encoding
+def getEncode(df, name, encoder):
+    encoder.fit(df[name])
+    labels = encoder.transform(df[name])
+    df.loc[:, name] = labels
+
+
+# onehot Encoding
+def onehotEncode(df, name):
+    le = preprocessing.OneHotEncoder(handle_unknown='ignore')
+    enc = df[[name]]
+    enc = le.fit_transform(enc).toarray()
+    le.categories_[0] = le.categories_[0].astype(np.str)
+    new = np.full((len(le.categories_[0]), 1), name + ": ")
+    le.categories_[0] = np.core.defchararray.add(new, le.categories_[0])
+    enc_df = pd.DataFrame(enc, columns=le.categories_[0][0])
+    df.reset_index(drop=True,inplace=True)
+    df = pd.concat([df, enc_df], axis=1)
+    df.drop(columns=[name], inplace=True)
+    return df
+
+
+# label encoding
 def labelEncode(df, name):
     encoder = preprocessing.LabelEncoder()
     encoder.fit(df[name])
     labels = encoder.transform(df[name])
     df.loc[:, name] = labels
 
-# get 2d array of dataframe with given dataframe
-def get_various_encode_scale(dataframe, numerical_columns, categorical_columns):
 
-    scale_Sd = preprocessing.StandardScaler()
-    scale_Mm = preprocessing.MinMaxScaler()
-    scale_Rb = preprocessing.RobustScaler()
 
-    encode_label = preprocessing.LabelEncoder
 
-    after_scale_encode = [[],[],[]]
-    group_dataframe = []
+"""
+Function to get 2d array of dataframe with given dataframe
+output: scale/encoded 2d array dataframe, scalers used, encoders used
+"""
+def get_various_encode_scale(X, numerical_columns, categorical_columns, scalers=None, encoders=None, scaler_name=None,
+                             encoder_name=None):
+    if categorical_columns is None:
+        categorical_columns = []
+    if numerical_columns is None:
+        numerical_columns = []
+
+    if len(categorical_columns) == 0:
+        return get_various_scale(X, numerical_columns, scalers, scaler_name)
+    if len(numerical_columns) == 0:
+        return get_various_encode(X, categorical_columns, encoders, encoder_name)
+
+    """
+    Test scale/encoder sets
+    """
+    if scalers is None:
+        scalers = [preprocessing.StandardScaler(), preprocessing.MinMaxScaler(), preprocessing.RobustScaler()]
+    if encoders is None:
+        encoders = [preprocessing.LabelEncoder(), preprocessing.OneHotEncoder()]
+
+    after_scale_encode = [[0 for col in range(len(encoders))] for row in range(len(scalers))]
+
     i = 0
-    j = 0
-    k = 0
-    tmp_result = dataframe.copy
-
-    while(k<3):
-        after_scale_encode[k].append(dataframe.copy())
-        after_scale_encode[k].append(dataframe.copy())
-        k+=1
+    for scale in scalers:
+        for encode in encoders:
+            after_scale_encode[i].pop()
+        for encode in encoders:
+            after_scale_encode[i].append(X.copy())
+        i = i + 1
 
     for name in numerical_columns:
-
-       scaled_Sd = scale_Sd.fit_transform(dataframe[name].values.reshape(-1,1))
-       scaled_Mm = scale_Mm.fit_transform(dataframe[name].values.reshape(-1,1))
-       scaled_Rb = scale_Rb.fit_transform(dataframe[name].values.reshape(-1,1))
-
-       k=0
-       while(k<2):
-          after_scale_encode[0][k][name] = scaled_Sd
-          after_scale_encode[1][k][name] = scaled_Mm
-          after_scale_encode[2][k][name] = scaled_Rb
-          k += 1
+        i = 0
+        for scaler in scalers:
+            j = 0
+            for encoder in encoders:
+                after_scale_encode[i][j][name] = scaler.fit_transform(X[name].values.reshape(-1, 1))
+                j = j + 1
+            i = i + 1
 
     for new in categorical_columns:
-        k = 0
-        while(k<3):
-           labelEncode(after_scale_encode[k][0], new)
-           onehotEncode(after_scale_encode[k][1], new)
-           k+=1
-    return after_scale_encode
+        i = 0
+        for scaler in scalers:
+            j = 0
+            for encoder in encoders:
+                if (str(type(encoder)) == "<class 'sklearn.preprocessing._label.LabelEncoder'>"):
+                    labelEncode(after_scale_encode[i][j], new)
+                elif (str(type(encoder)) == "<class 'sklearn.preprocessing._encoders.OneHotEncoder'>"):
+                    after_scale_encode[i][j] = onehotEncode(after_scale_encode[i][j], new)
+                else:
+                    getEncode(after_scale_encode[i][j], new, encoder)
+                j = j + 1
+            i = i + 1
+
+    return after_scale_encode, scalers, encoders
 
 
+"""
+If there aren't categorical value, do this function
+This function only scales given X
+Output: 1d array of scaled dataset, scalers used, encoders used(Nothing)
+"""
+def get_various_scale(X, numerical_columns, scalers=None, scaler_name=None):
+    """
+    Test scale/encoder sets
+    """
+    if scalers is None:
+        scalers = [preprocessing.StandardScaler(), preprocessing.MinMaxScaler(), preprocessing.RobustScaler()]
+        # scalers = [preprocessing.StandardScaler()]
+    encoders = ["None"]
+
+    after_scale = [[0 for col in range(1)] for row in range(len(scalers))]
+
+    i = 0
+    for scale in scalers:
+        for encode in encoders:
+            after_scale[i].pop()
+        for encode in encoders:
+            after_scale[i].append(X.copy())
+        i = i + 1
+
+    for name in numerical_columns:
+        i = 0
+        for scaler in scalers:
+            after_scale[i][0][name] = scaler.fit_transform(X[name].values.reshape(-1, 1))
+            i = i + 1
+
+    return after_scale, scalers, ["None"]
+
+
+"""
+If there aren't numerical value, do this function
+This function only encodes given X
+Return: 1d array of encoded dataset, scalers used(Nothing), encoders used
+"""
+def get_various_encode(X, categorical_columns, encoders=None, encoder_name=None):
+    """
+    Test scale/encoder sets
+    """
+    if encoders is None:
+        encoders = [preprocessing.LabelEncoder(),preprocessing.OneHotEncoder()]
+        # encoders = [preprocessing.LabelEncoder()]
+    scalers = ["None"]
+
+    after_encode = [[0 for col in range(1)] for row in range(len(scalers))]
+
+    i = 0
+    for scale in scalers:
+        for encode in encoders:
+            after_encode[i].pop()
+        for encode in encoders:
+            after_encode[i].append(X.copy())
+        i = i + 1
+
+    for new in categorical_columns:
+        j = 0
+        for encoder in encoders:
+            if (str(type(encoder)) == "<class 'sklearn.preprocessing._label.LabelEncoder'>"):
+                labelEncode(after_encode[0][j], new)
+            elif (str(type(encoder)) == "<class 'sklearn.preprocessing._encoders.OneHotEncoder'>"):
+                after_encode[0][j] = onehotEncode(after_encode[0][j], new)
+            else:
+                getEncode(after_encode[0][j], new, encoder)
+            j = j + 1
+
+    return after_encode, ["None"], encoders
 
